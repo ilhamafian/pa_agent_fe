3799d06f3fcefd9ab5759bdfd7877dc3ee566639
ILHAM GHAZI BIN ISKANDAR AFIAN <ilhamghazi@hopexito.com>
2025-07-27 04:10:29 +0800

    fisrt commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { children } = $props();
+    import "../app.css";
+  </script>
+  <section class="max-w-xl mx-auto min-h-screen border-2 border-sky-200 rounded-md p-4">
+      {@render children()}
+    </section>
---
+++ src/routes/redirect/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Redirect to Whatsapp</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
