fb9c671a567d2a2907962f0c3e0f1dce83f810aa
ILHAM GHAZI BIN ISKANDAR AFIAN <ilhamghazi@hopexito.com>
2025-07-27 05:32:56 +0800

    ui changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
-    let { children } = $props();
-    import "../app.css";
-  </script>
-  <section class="max-w-xl mx-auto min-h-screen border-2 border-sky-200 rounded-md p-4">
-      {@render children()}
-    </section>
+  let { children } = $props();
+  import "../app.css";
+</script>
+
+<section class="bg-emerald-100">
+  <div class="max-w-xl mx-auto min-h-screen p-6">
+    {@render children()}
+  </div>
+</section>
--- src/routes/auth-result/+page.svelte
+++ src/routes/auth-result/+page.svelte
@@ -1,17 +1,61 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+  import { page } from "$app/stores";
 
-	$: url = $page.url;
-	$: status = url.searchParams.get('status'); // 'success' or 'error'
-	$: reason = url.searchParams.get('reason'); // optional
+  $: url = $page.url;
+  $: status = url.searchParams.get("status"); // 'success' or 'error'
+  $: reason = url.searchParams.get("reason"); // optional
 </script>
 
-<h1 class="text-2xl font-bold">Google OAuth Result</h1>
+<h1 class="text-2xl font-bold text-center">Google OAuth Result</h1>
 
-{#if status === 'success'}
-	<p class="text-green-600">✅ Login successful! You're all set.</p>
-{:else if status === 'error'}
-	<p class="text-red-600">❌ Login failed. Reason: {reason ?? 'unknown error'}</p>
+{#if status === "success"}
+  <div
+    class="flex flex-col gap-4 border-2 border-emerald-500 rounded-xl max-h-78 text-center p-4 m-2"
+  >
+    <p class="text-gray-800">Login successful! You're all set.</p>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="size-12 mx-auto text-emerald-500"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+    <a
+      href="https://wa.me/15550437601"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="px-4 py-2 cursor-pointer hover:bg-emerald-400 ring-2 ring-emerald-400 bg-transparent rounded-md min-w-48 mx-auto block text-center"
+    >
+      <span class="flex gap-3 items-center justify-center">
+        <p>Redirect to WhatsApp</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+          />
+        </svg>
+      </span>
+    </a>
+  </div>
+{:else if status === "error"}
+  <p class="text-red-600">
+    ❌ Login failed. Reason: {reason ?? "unknown error"}
+  </p>
 {:else}
-	<p>Waiting for redirect result...</p>
+  <p>Waiting for redirect result...</p>
 {/if}
