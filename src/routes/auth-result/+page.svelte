609a877c65660c7b5d4c6effa992ff88b4a9c8ad
ILHAM GHAZI BIN ISKANDAR AFIAN <ilhamghazi@hopexito.com>
2025-08-11 15:31:56 +0800

    Update WhatsApp link text and add dashboard navigation; remove commented-out code for cleaner structure.

--- src/routes/auth-result/+page.svelte
+++ src/routes/auth-result/+page.svelte
@@ -14,15 +14,15 @@
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-12 mx-auto text-emerald-500">
       <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
     </svg>
-    <!-- <a href="https://wa.me/15550437601" target="_blank" rel="noopener noreferrer" class="px-4 py-2 cursor-pointer hover:bg-emerald-400 ring-2 ring-emerald-400 bg-transparent rounded-md min-w-48 mx-auto block text-center">
+    <a href="https://wa.me/15550437601" target="_blank" rel="noopener noreferrer" class="px-4 py-2 cursor-pointer hover:bg-emerald-400 ring-2 ring-emerald-400 bg-transparent rounded-md min-w-48 mx-auto block text-center">
       <span class="flex gap-3 items-center justify-center">
-        <p>Redirect to WhatsApp</p>
+        <p>Chat with Lofy on WhatsApp</p>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
         </svg>
       </span>
-    </a> -->
-    <a href="https://wa.me/15550437601" target="_blank" rel="noopener noreferrer" class="px-4 py-2 cursor-pointer hover:bg-emerald-400 ring-2 ring-emerald-400 bg-transparent rounded-md min-w-48 mx-auto block text-center">
+    </a>
+    <a href="/dashboard" rel="noopener noreferrer" class="px-4 py-2 cursor-pointer hover:bg-emerald-400 ring-2 ring-emerald-400 bg-transparent rounded-md min-w-48 mx-auto block text-center">
       <span class="flex gap-3 items-center justify-center">
         <p>Dashboard</p>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
